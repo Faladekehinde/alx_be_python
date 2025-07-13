7427b823f4a3e433eb4bc1633be711a9ae18d31a
Faladekehinde <dafaladexy@gmail.com>
2025-07-13 23:01:33 +0100

    Updating Simple interest file

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,10 +1,10 @@
-#Define the variables
+# Define the variables
 principal = 1000 # representing $1000
-rate = 5 # representing 5% annual interest rate
+rate = 0.05 # representing 5% annual interest rate
 time = 3 # representing 3 years
 
-#Calculate the simple interest
-The_simple_interest = principal * rate * time
+# Calculate the simple interest
+interest = principal * rate * time
 
-#Print the simple interest
-print("The simple interest is:",The_simple_interest)
+# Print the simple interest
+print("The simple interest is:",interest)
