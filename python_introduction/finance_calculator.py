1fcbbc1161997355c6e4710899eb81c62ab5df69
Faladekehinde <dafaladexy@gmail.com>
2025-07-13 21:54:12 +0100

    Fix: correct input prompt to match ALX checker

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,15 @@
+# Prompt the user for financial details
+income = float(input("Enter your monthly income: "))
+expenses = float(input("Enter your total monthly expenses: "))
+
+# Calculate monthly savings
+monthly_savings = income - expenses
+
+# Calculate annual savings with 5% interest
+annual_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
+
+# Output the results
+print("Your monthly savings are $" + str(int(monthly_savings)) + ".")
+print("Projected savings after one year, with interest, is: $" + str(int(annual_savings)) + ".")
+
+
